936916feb06f4adb888ea3e4c021d63b98c21c20
Andres Ravinet <39545334+andresravinet@users.noreply.github.com>
2020-09-16 23:19:01 -0400

    Update Program.cs

--- apis/poi/web/Program.cs
+++ apis/poi/web/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using poi.Utility;
-testfailure
 namespace poi
 {
     public class Program
